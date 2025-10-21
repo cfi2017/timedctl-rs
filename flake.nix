05be433ba684f5fe8a880bd28bba5ef4e4d88adb
Carlo Field <carlo.field@adfinis.com>
2025-10-21 10:36:51 +0200

    feat: add flake (#86)

---
+++ flake.nix
@@ -0,0 +1,43 @@
+{
+  description = "timedctl-rs nix flake";
+
+  inputs = {
+    nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
+    flake-utils.url = "github:numtide/flake-utils";
+    rust-overlay.url = "github:oxalica/rust-overlay";
+  };
+
+  outputs = { self, nixpkgs, flake-utils, rust-overlay }: 
+    flake-utils.lib.eachDefaultSystem (system: 
+      let
+        overlays = [ (import rust-overlay )];
+        pkgs = import nixpkgs {
+          inherit system overlays;
+        };
+        rustToolchain = pkgs.rust-bin.nightly.latest.default.override {
+          extensions = [ "rust-src" "rust-analyzer" ];
+        };
+      in
+      {
+        devShells.default = pkgs.mkShell {
+          buildInputs = with pkgs; [
+            rustToolchain
+            pkg-config
+          ];
+        };
+
+        packages.default = (pkgs.makeRustPlatform {
+          cargo = rustToolchain;
+          rustc = rustToolchain;
+        }).buildRustPackage {
+          pname = "timedctl";
+          version = "0.1.0";
+          src = ./.;
+          cargoLock.lockFile = ./Cargo.lock;
+
+          nativeBuildInputs = [ pkgs.pkg-config ];
+          # buildInputs = [ pkgs.openssl ];
+        };
+      }
+    );
+}
